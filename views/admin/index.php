32e098937c6400b39edccff2faff0ee00f740e36
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-04-08 21:43:49 -0400

    Update index.php

--- views/admin/index.php
+++ views/admin/index.php
@@ -1,15 +1,15 @@
 <?php
 
-use yii\bootstrap\ActiveForm;
-use yii\helpers\Html;
-use yii\helpers\Url;
+use humhub\compat\CActiveForm;
+use humhub\compat\CHtml;
+use humhub\models\Setting;
 use humhub\modules\discord\controllers\AdminController;
 ?>
 
 <div class="panel panel-default">
 	<div class="panel-heading"><?= Yii::t('DiscordModule.base', '<strong>Discord</strong>'); ?></div>
 	<div class="panel-body">
-		<?php $form = ActiveForm::begin(['id' => 'discord-settings-form']); ?>
+		<?php $form = CActiveForm::begin(['id' => 'discord-settings-form']); ?>
 			<?= $form->errorSummary($model); ?>
 			<p class="help-block"><?= Yii::t('DiscordModule.base', 'eg:  "99999999"'); ?></p>
 			<div class="form-group">
@@ -17,8 +17,8 @@ use humhub\modules\discord\controllers\AdminController;
 				<?= $form->textField($model, 'sort', ['class' => 'form-control', 'readonly' => Setting::IsFixed('sort', 'discord')]); ?>
 			</div>
 			<p class="help-block"><?= Yii::t('DiscordModule.base', 'Widget positioning.'); ?></p>
-			<?= Html::submitButton(Yii::t('DiscordModule.base', 'save'), ['class' => 'btn btn-primary']); ?>
+			<?= CHtml::submitButton(Yii::t('DiscordModule.base', 'save'), ['class' => 'btn btn-primary']); ?>
 			<?= \humhub\widgets\DataSaved::widget(); ?>
-		<?php ActiveForm::end(); ?>
+		<?php CActiveForm::end(); ?>
 	</div>
 </div>
